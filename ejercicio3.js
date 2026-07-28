16464a1065691c6045efe0462a73b2060a9cbb82
cristinaWeb <cristinamembreno68@gmail.com>
2026-07-28 16:53:18 -0600

    Agrego ejercicios resueltos

--- ejercicio3.js
+++ ejercicio3.js
@@ -7,5 +7,9 @@ const rl = readline.createInterface({
 //3- Elabore un programa que solicite una cantidad de horas y muestre su equivalencia en minutos.
 
 rl.question ("Ingrese la cantidad de horas: ",function(horas){
-    
+    const minutos = Number(horas) * 60;
+
+    console.log(`${horas} horas equivalen a ${minutos} minutos`);
+
+    rl.close();
 })
--- ejercicio4.js
+++ ejercicio4.js
@@ -6,3 +6,16 @@ const rl = readline.createInterface({
 
 //4- Elabore un programa que solicite el nombre de un producto, su precio unitario y la cantidad comprada. 
 // Luego, calcule el total que debe pagar el cliente.
+
+rl.question("Ingrese el nombre del producto: ", function(producto) {
+    rl.question("Ingrese el precio unitario: ", function(precio) {
+        rl.question("Ingrese la cantidad comprada: ", function(cantidad) {
+            const total = Number(precio) * Number(cantidad);
+
+            console.log(`Producto: ${producto}`);
+            console.log(`El total a pagar es: $${total}`);
+
+            rl.close();
+        });
+    });
+});
